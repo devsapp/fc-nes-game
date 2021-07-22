daa598d0f2745d3b471d7fefca2ba300dbcd6ee0
anycodes <jiangyu.ly@alibaba-inc.com>
2021-07-22 13:53:43 +0800

    fix output

--- hook/index.js
+++ hook/index.js
@@ -3,20 +3,19 @@ async function preInit(inputObj) {
 }
 
 async function postInit(inputObj) {
-    console.log(`\n     _______  _______  ______   _______  ___      ___   _______  _______ 
-    |       ||       ||      | |       ||   |    |   | |       ||       |
-    |_     _||   _   ||  _    ||   _   ||   |    |   | |  _____||_     _|
-      |   |  |  | |  || | |   ||  | |  ||   |    |   | | |_____   |   |  
-      |   |  |  |_|  || |_|   ||  |_|  ||   |___ |   | |_____  |  |   |  
-      |   |  |       ||       ||       ||       ||   |  _____| |  |   |  
-      |___|  |_______||______| |_______||_______||___| |_______|  |___|  
-                                        `)
-    console.log(`\n    Welcome to the todolist-app application
+    console.log(`    \n____________________     ________    _____      _____  ___________
+    \\_   _____/\\_   ___ \\   /  _____/   /  _  \\    /     \\ \\_   _____/
+     |    __)  /    \\  \\/  /   \\  ___  /  /_\\  \\  /  \\ /  \\ |    __)_ 
+     |     \\   \\     \\____ \\    \\_\\  \\/    |    \\/    Y    \\|        \\
+     \\___  /    \\______  /  \\______  /\\____|__  /\\____|__  /_______  /
+         \\/            \\/          \\/         \\/         \\/        \\/ 
+     `)
+    console.log(`\n    Welcome to the fc-nes-game application
      This application requires to open these services: 
          FC : https://fc.console.aliyun.com/
-     This application can help you quickly deploy the todolist-app project.
+     This application can help you quickly deploy the fc-nes-game project.
      The application uses FC component：https://github.com/devsapp/fc
-     The application homepage: https://github.com/devsapp/todolist\n`)
+     The application homepage: https://github.com/devsapp/fc-nes-game\n`)
 }
 
 module.exports = {
